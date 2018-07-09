6522640343b7bef9217b5e2ab0bac16c1926fa53
munirkizilcali <munir.kizilcali@gmail.com>
2018-07-09 14:11:35 -0400

    add user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_06_171050) do
+ActiveRecord::Schema.define(version: 2018_07_09_180801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,14 @@ ActiveRecord::Schema.define(version: 2018_07_06_171050) do
     t.string "thumbnail"
     t.string "artworks_link"
     t.string "artists_link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
